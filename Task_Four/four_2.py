2ff1ba5cbc9063daaa76f633cb206f83be63796d
swathiganji <62779591+swathiganji@users.noreply.github.com>
2022-01-07 09:45:36 -0500

    Update four_2.py

--- Task_Four/four_2.py
+++ Task_Four/four_2.py
@@ -1,12 +1,13 @@
+def example():
+    x = input("Enter a word: ")
+    u = 0
+    l = 0
+    for i in x:
+        if i == i.upper():
+            u += 1
+        elif i == i.lower():
+            l += 1
 
-x = input("Enter a word: ")
-u = 0
-l = 0
-for i in x:
-    if i == i.upper():
-        u += 1
-    elif i == i.lower():
-        l += 1
-
-print("No. of Uppercase characters: ",u , "No. of Lower case Characters: ", l)
+    print("No. of Uppercase characters: ",u , "No. of Lower case Characters: ", l)
+example()
 
