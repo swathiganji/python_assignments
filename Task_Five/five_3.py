7ce6fcaf2af06c599ce30ceb5c4fff408ac9f5a7
Sreedhar Ganji <swathi.ganji88@gmail.com>
2022-12-20 10:17:54 -0500

    added a new file

---
+++ Task_Five/five_3.py
@@ -0,0 +1,9 @@
+import sys
+filename = sys.argv[]
+file = open(filename, 'r')
+lines = file.readlines()
+for line in lines:
+    print(line)
+
+###This is a comment to test code push
+###This is a comment to test code push
